b88d28245181ed4234686e2877e9b954df26be5f
Pierre-Henry Soria <hi@ph7.me>
2025-11-26 22:26:52 +1100

    refactor: Remove membershipType as won't be needed to identify/authenticate users

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -23,7 +23,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       userId: payload.sub,
       email: payload.email,
       name: payload.name,
-      membershipType: payload.membershipType,
     };
   }
 }
